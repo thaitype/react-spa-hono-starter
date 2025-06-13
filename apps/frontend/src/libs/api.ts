ad81c969e54dd69eb4c185aca1eb1f1282db561a
Thada Wangthammang <thada.wth@gmail.com>
2025-06-13 21:45:44 +0700

    fix: update API client path to use '/api' for correct routing

--- apps/frontend/src/libs/api.ts
+++ apps/frontend/src/libs/api.ts
@@ -1,6 +1,6 @@
 import { hc } from "hono/client";
 import { type ApiRoutes } from "@acme/server";
-import { queryOptions } from "@tanstack/react-query";
-// import { type CreateExpense } from "@server/sharedTypes";
 
-const client = hc<ApiRoutes>("/");
+const client = hc<ApiRoutes>("/api");
+
+export const api = client.api;
